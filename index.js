7dfd5fe99332de6ab86e9899e5a1294dea7e5e4d
manikanta <smanikanta515@gmail.com>
2024-11-28 14:13:22 +0530

    adding cors1

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const path = require("path");
-
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
+const cors = require('cors');
+app.use(cors())
 const app = express();
 
 const dbPath = path.join(__dirname, "fooditems.db");
