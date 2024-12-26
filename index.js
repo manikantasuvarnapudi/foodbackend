0eb690c578724416974dd2a22c2b79da8e811381
manikanta <smanikanta515@gmail.com>
2024-12-26 17:11:01 +0530

    user table added

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const twilio = require("twilio");
 const { v4: uuidv4 } = require('uuid');
-
-
+const jsonMiddleware = express.json();
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
@@ -15,7 +14,8 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const cors = require('cors');
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(jsonMiddleware);
+
 
 
 
@@ -49,7 +49,8 @@ const initializeDBAndServer = async () => {
 
     await db.run(`DROP TABLE IF EXISTS orders;`);
     await db.run(`DROP TABLE IF EXISTS fooditems;`);
-    await db.run("DELETE FROM sqlite_sequence WHERE name='fooditems' AND name='orders';");
+    await db.run(`DROP TABLE IF EXISTS users;`);
+    await db.run("DELETE FROM sqlite_sequence WHERE name='fooditems' AND name='orders' and name='users';");
 
     await db.run(`
       CREATE TABLE IF NOT EXISTS fooditems (
@@ -73,6 +74,19 @@ const initializeDBAndServer = async () => {
           completedtime TEXT NOT NULL
       )
     `);
+   
+    await db.run(`
+      CREATE TABLE IF NOT EXISTS users (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          username TEXT NOT NULL UNIQUE,
+          name TEXT NOT NULL,
+          password TEXT NOT NULL,
+          gender TEXT NOT NULL,
+          location TEXT NOT NULL
+      )
+    `);
+    
+
     const checkDataQuery = `SELECT COUNT(*) AS count FROM fooditems;`;
     const { count } = await db.get(checkDataQuery)
     if (count === 0) {
@@ -177,6 +191,33 @@ app.get("/orders", async (req, res) => {
   const updateOrders = orders.map((each) => ({ ...each, orderDetails: JSON.parse(each.orderDetails) }))
   res.send(updateOrders)
 })
+
+// Endpoint for user registration
+app.post("/users/", async (request, response) => {
+  const { username, name, password, gender, location } = request.body;
+  const hashedPassword = await bcrypt.hash(request.body.password, 10);
+  const selectUserQuery = `SELECT * FROM users WHERE username = '${username}'`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) {
+    const createUserQuery = `
+      INSERT INTO 
+        user (username, name, password, gender, location) 
+      VALUES 
+        (
+          '${username}', 
+          '${name}',
+          '${hashedPassword}', 
+          '${gender}',
+          '${location}'
+        )`;
+    const dbResponse = await db.run(createUserQuery);
+    const newUserId = dbResponse.lastID;
+    response.send(`Created new user with ${newUserId}`);
+  } else {
+    response.status = 400;
+    response.send("User already exists");
+  }
+});
 
 
 // Endpoint to send an OTP
