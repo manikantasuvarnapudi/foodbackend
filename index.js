561a2c53a11a7c21aebce0d78a9c4a8ff292b162
manikanta <smanikanta515@gmail.com>
2025-01-11 11:10:09 +0530

    twilio newcreadentials2

--- index.js
+++ index.js
@@ -291,7 +291,7 @@ ${otp} is your OTP for ScreenBites. Please do not share it with anyone.
 
 @screenbites.vercel.app #${otp}
 `,
-      from: "+919000046661",
+      from: "+19000046661",
       to: `+91${phone}`,
 
     });
